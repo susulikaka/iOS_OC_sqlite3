8de3a7b4c5fa30ab2a3c19f2083b7a639ec10091
lisuping <lisuping@lukou.com>
2016-07-21 14:26:58 +0800

    Initial Commit

---
+++ SQLITE3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQLITE3
+//
+//  Created by admin on 16/7/21.
+//  Copyright © 2016年 SupingLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
